d63707514b1b134a211bd4988afc2bf13c02f13d
tlemar00 <tlemar00@estudiantes.unileon.es>
2019-03-18 12:11:30 +0100

    initial commit

---
+++ src/es/unileon/prg/practicaDate/Date.java
@@ -0,0 +1,17 @@
+package es.unileon.prg.practicaDate;
+
+public class Date {
+
+	//Declaracion de variables
+	private int day;
+	private int month;
+	private int year;
+
+	//Constructor
+	private Date(int day, int month, int year){
+		_day = day;
+		_month = month;
+		_year = year;
+	}
+
+}
---
+++ src/es/unileon/prg/practicaDate/MainDate.java
@@ -0,0 +1,14 @@
+package es.unileon.prg.practicaDate;
+
+public class MainDate {
+
+	public static void main(String[] args) {
+
+		Date today, tomorrow;
+
+		today = new Date(18, 03, 2019);
+		tomorrow = new Date(19, 03, 2010);
+		
+	}
+
+}

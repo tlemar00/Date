ff4cc409312faf8df9020515b9c402b4e9d2fb5b
tlemar00 <tlemar00@estudiantes.unileon.es>
2019-03-23 17:27:38 +0100

    codigo actualizado con algunos errores

--- src/es/unileon/prg/practicaDate/Date.java
+++ src/es/unileon/prg/practicaDate/Date.java
@@ -10,45 +10,126 @@ public class Date {
 	//Constructor
 	public Date(int day, int month, int year) throws DateException{
 	
-	this.year = year;
-	
-	if (month < 1 || month > 12) {
-		throw new DateException("Mes " + month + " no valido" +
-				" Valores posibles entre 1 y 12.");
-	} else {
-		this.month = month;
-	}
-	this.day = day;
+		this.year = year;
+		
+		if (month < 1 || month > 12) {
+			throw new DateException("Mes " + month + " no valido. Valores posibles entre 1 y 12.");
+		} else {
+			this.month = month;
+		}
+
+		if (day < 1 || day > 31){
+			throw new DateException("Día " + day + " no válido. Valores posibles entre 1 y 31.");
+		}else{
+			this.day = day;
+		}
 	}
 
+	public int getYear(int year){
+		return this.year;
+	}
+
+	//Comprobamos si es el mismo anio
+	public boolean isSameYear(Date tomorrow){
+
+		boolean mismoAnio;
+
+		if(this.year == tomorrow.getYear()){
+			mismoAnio = true;
+			System.out.println("Tienen el mismo año.");
+		}else{
+			mismoAnio = false;
+			System.out.println("NO tienen el mismo año.");
+		}
+
+		return mismoAnio;
+	}
+
+	public int getMonth(int month){
+		return this.month;	
+	}
+
+	//Comprobamos si es el mismo mes
+	public boolean isSameMonth(Date tomorrow){
+
+		boolean mismoMes;
+
+		if(this.month == tomorrow.getMonth()){
+			mismoMes = true;
+			System.out.println("Tienen el mismo mes.");
+		}else{
+			mismoMes = false;
+			System.out.println("NO tienen el mismo mes.");
+		}
+
+		return mismoMes;
+	}
+
+	public int getDay(int day){
+		return this.day;	
+	}
+
+	//Comprobamos si es el mismo dia
+	public boolean isSameDay(Date tomorrow){
+
+		boolean mismoDia;
+
+		if(this.day == tomorrow.getDay()){
+			mismoDia = true;
+			System.out.println("Tienen el mismo día.");
+		}else{
+			mismoDia = false;
+			System.out.println("NO tienen el mismo día.");
+		}
+
+		return mismoDia;
+	}
+
+	//Comprobamos si es la misma fecha
+	public boolean isSame(Date tomorrow){
+
+		boolean igual;
+
+		if((isSameDay() == true) && (isSameMonth() == true) && (isSameYear() == true)){
+			igual = true;
+			System.out.println("Es la misma fecha.");
+		}else{
+			igual = false;
+			System.out.println("No es la misma fecha.");
+		}
+
+		return igual;
+	}
+
+	/*
+	//Metodo toString
 	public String toString() {
 		return this.day + "/" + this.month + "/" + this.year;
 	}
 	
-	//
-	public boolean isSameYear(int year){
-	
-		return (this.year == year);
-	}
-	
+	//Comprobamos si es el mismo mes
 	public boolean isSameMonth(int month){
 	
 		return (this.month == month);
 	}
 	
+	//Comprobamos si el mismo dia
 	public boolean isSameDay(int day){
 
 		return (this.day == day);
 	}
 
+	//Comprobamos si es la misma fecha
 	public boolean isSame(){
 
 		return true;
 	}
+	*/
 
+	//Devuelve el dia del mes
 	public int getDayOfMonth(int day, int month){
 
-		int v_dias_del_mes = 0;
+		int dias_del_mes = 0;
 
 		switch(month){
 
@@ -59,22 +140,23 @@ public class Date {
 			case 8:
 			case 10:
 			case 12:
-				v_dias_del_mes = 31;
+				dias_del_mes = 31;
 				break;
 			case 2:
-				v_dias_del_mes = 28;
+				dias_del_mes = 28;
 				break;
 			case 4:
 			case 6:
 			case 9:
-				v_dias_del_mes = 30;
+				dias_del_mes = 30;
 				break;
 			default: 
 				break;
 		}
-		return v_dias_del_mes;
+		return dias_del_mes;
 	}
 
+	//Devuelve el nombre del mes
 	public String getMonthName(int month){
 
 		String v_s_retorno = "ERROR";
@@ -123,6 +205,26 @@ public class Date {
 		return v_s_retorno;
 	}
 
+	public String SeasonName(int day, month){
+
+		String season;
+
+		if(((day >= 21) && (month >= 3)) && (( day < 21 ) && (month <= 6))){
+			season = "Spring";
+		}
+
+		if(((day >= 21) && (month >= 6)) && ((day < 23) && (month <= 9))){
+			season = "Summer";
+		}
+
+		if(((day >= 23) && (month >= 9)) && ((day < 21) && (month <= 12))){
+			season = "Autumn";
+		}
+		
+		if(((day >= 21) && (month >= 12)) && ((day < 21) && (month <= 3))){
+			season = "Winter";
+		}
+		return season;
+	}
 
 }
-
--- src/es/unileon/prg/practicaDate/MainDate.java
+++ src/es/unileon/prg/practicaDate/MainDate.java
@@ -5,10 +5,13 @@ public class MainDate {
 	public static void main(String[] args) {
 
 		Date today;
+		Date tomorrow;
 
 		try{
 			today = new Date(18, 03, 2019);
 			System.out.println(today.toString());
+			tomorrow = new Date(19, 03, 2019);
+			System.out.println(tomorrow.toString());
 		}catch(DateException e){
 			System.out.println(e.getMessage());
 		}
